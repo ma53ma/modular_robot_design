ce3c3432dc856070042087bd7917e61964faf778
Max Asselmeier <ma53@illinois.edu>
2020-06-30 16:17:37 -0500

    loss now showing exponential decay, results are mostly within > 1cm

--- buffer_dqn.py
+++ buffer_dqn.py
@@ -32,11 +32,11 @@ with open('mini_mods.csv') as f:
 class DQN(nn.Module):
     def __init__(self, state_size, action_size, lr, gamma, target_size):
         super(DQN, self).__init__()
-        self.goal_layer = nn.Linear(target_size, 6)
+        self.goal_layer = nn.Linear(target_size, 9)
         self.a_layer = nn.Linear(state_size, 64)
-        self.model = nn.Sequential(nn.Linear(70, 64), nn.ReLU(), nn.Linear(64, 32),
+        self.model = nn.Sequential(nn.Linear(73, 128), nn.ReLU(), nn.Linear(128, 64),nn.ReLU(), nn.Linear(64, 32),
                                     nn.ReLU(), nn.Linear(32, action_size))
-        self.optimizer = optim.Adam(self.parameters(),lr)
+        #self.optimizer = optim.Adam(self.parameters(),lr)
         self.loss_fn = nn.MSELoss()
         self.lr = lr
         self.gamma = gamma
@@ -57,8 +57,8 @@ class env():
     def __init__(self, arm_size):
         a = np.array([0, 0, 0, 0, 0, 0] * arm_size)
         self.state = torch.from_numpy(a).type(torch.FloatTensor)
-        goal = np.array([random.uniform(-.3, .3), random.uniform(-.3, .3),
-                         random.uniform(-.3, .3)])
+        goal = np.array([random.uniform(-.5, 0.5), random.uniform(-.5, 0.5),
+                         random.uniform(0.0, 0.5)])
         self.goal = torch.from_numpy(goal).type(torch.FloatTensor)
         self.arm_size = arm_size
         self.prev_action = 1
@@ -70,31 +70,33 @@ class env():
                         torch.from_numpy(np.array([0, 0, 0, 0, 1, 0])).type(torch.FloatTensor),
                         torch.from_numpy(np.array([0, 0, 0, 0, 0, 1])).type(torch.FloatTensor)]
         self.mod_size = len(self.actions)
-        self.batch_size = 50
+        self.batch_size = 100
+
         self.buffer_goal = []
         self.buffer_reward = []
         self.failed = 0
         self.sequence = []
+        self.epsilon = .05
 
     def reset(self):
         a = np.array([0, 0, 0, 0, 0, 0] * self.arm_size)
         self.state = torch.from_numpy(a).type(torch.FloatTensor)
         self.prev_action = 1
-        goal = np.array([random.uniform(-.3, .3), random.uniform(-.3, .3),
-                         random.uniform(-.3, .3)])
+        goal = np.array([random.uniform(-0.5, 0.5), random.uniform(-0.5, 0.5),
+                         random.uniform(0.0, 0.5)])
         self.goal = torch.from_numpy(goal).type(torch.FloatTensor)  # randomizing location of goal
         self.buffer_goal = []
         self.sequence = []
         self.failed = 0
 
-
     def step(self, a, curr, goal, action):
         global steps_done
         done = 0
-
+        mask = []
         steps_done += 1
-        if action == self.mod_size - 1:
-            done = 1
+        if action > 0:
+            if action == self.mod_size - 1:
+                done = 1
         # storing previous action
         self.prev_action = action
 
@@ -103,26 +105,39 @@ class env():
         next_a[curr: curr + self.mod_size] = self.actions[action]
 
         # obtaining reward for new arrangement and distance from goal if terminal arrangement
-        r, dist, pos  = reward(next_a, curr, self.mod_size, goal, action, modules)
+        r, dist, pos  = reward(next_a, curr, self.mod_size, goal, action, modules, env)
 
         if done:
-            if dist > .05:
+            if dist > self.epsilon:
                 self.buffer_goal = torch.from_numpy(np.array(pos)).type(torch.FloatTensor)
                 self.failed = 1
 
-        return next_a, r, dist, done
+        if action == 0:
+            mask = torch.zeros(1, self.mod_size, dtype=torch.bool)
+            mask[0][0:1] = True
+            #print('mask: ', mask)
+        else:
+            mask = torch.zeros(1, self.mod_size, dtype=torch.bool)
+            mask[0][1:self.mod_size] = True
+            #print('mask: ', mask)
 
-    def test_step(self, target_net, curr):
-        values = target_net.forward(self.state, self.goal, 0).detach().numpy()
-        qvals = masking(values, self.mod_size, self.prev_action)
-        print('qvals: ', qvals)
+        return next_a, r, dist, done, mask
+
+    def test_step(self, policy_net, curr):
+        #sum = 0
+        qvals = policy_net.forward(self.state, self.goal, 0).detach().numpy()
+        #print('test step values: ', qvals)
+        #for i in range(self.mod_size):
+        #    qvals[i] = np.exp(qvals[i])
+        qvals = masking(qvals, self.mod_size, self.prev_action)
+        #print('qvals: ', qvals)
         action = np.argmax(qvals)
         self.prev_action = action
         self.state[curr: curr + self.mod_size] = self.actions[action]
         return action
 
 Transition = namedtuple('Transition',
-                        ('state', 'action', 'next_state', 'reward', 'goal', 'done'))
+                        ('state', 'action', 'next_state', 'reward', 'goal', 'done', 'mask'))
 
 class ReplayMemory():
 
@@ -145,70 +160,95 @@ class ReplayMemory():
 
 def validation(env):
     for goal in val_goals:
+        print('for validation goal: ', goal)
         for curr in range(0, len(env.state), env.mod_size):
-            action = env.test_step(target_net, curr)
+            action = env.test_step(policy_net, curr)
             if action == env.mod_size - 1:
-                final_dist = term_reward(env.state, env.mod_size, goal, curr, modules)
+                final_dist = term_reward(env.state, env.mod_size, goal, curr, modules, env)
                 test_a = env.state.numpy()
                 print('')
-                print('for validation goal: ', goal)
                 print('arrangement: ', test_a)
                 print('distance: ', final_dist[0])
                 print('')
                 if goal == val_goals[0]:
                     writer.add_scalar('Validation Distance/(.1,.1,.1)', final_dist[0], ep)
                 elif goal == val_goals[1]:
+                    writer.add_scalar('Validation Distance/(.2,.2,.2)', final_dist[0], ep)
+                elif goal == val_goals[2]:
                     writer.add_scalar('Validation Distance/(.3,.3,.3)', final_dist[0], ep)
                 else:
-                    writer.add_scalar('Validation Distance/(.5,.5,.5)', final_dist[0], ep)
+                    writer.add_scalar('Validation Distance/(.4,.4,.4)', final_dist[0], ep)
                 break
         env.reset()
 
-def optimize_model(buffer, env, policy_net, target_net):
+def optimize_model(buffer, env, policy_net, target_net, optimizer):
     if len(buffer) < env.batch_size:
         return 0
     transitions = buffer.sample(env.batch_size)
     batch = Transition(*zip(*transitions))
     state_batch = torch.cat(batch.state).view(env.batch_size,len(env.state))
     action_batch = torch.cat(batch.action).view(env.batch_size,1)
-    next_state_batch = torch.cat(batch.next_state).view(env.batch_size,len(env.state))
-    reward_batch = torch.cat(batch.reward)
     goal_batch = torch.cat(batch.goal).view(env.batch_size,len(env.goal))
+    mask_batch = torch.cat(batch.mask).view(env.batch_size, env.mod_size)
+    #curr_mask_batch = torch.cat(batch.curr).view(env.batch_size, env.mod_size)
+    #print('curr mask batch: ', curr_mask_batch)
+    #curr_mask_transposed = torch.transpose(curr_mask_batch, 0, 1)[0].unsqueeze(1)
+    #print('curr mask transposed: ', )
+    #print('mask: ', mask_batch)
+    #print('states: ', state_batch.unsqueeze(0))
+    #print('actions: ', action_batch.unsqueeze(0))
+    #print('goals: ', goal_batch.unsqueeze(0))
+    #print('state size: ', state_batch.size())
+    #print('goal size: ', goal_batch.size())
+    state_action_values = policy_net(state_batch, goal_batch, 1).gather(1, action_batch)
+    #final_state_values = state_action_values[curr_mask_batch]
+    #print('')
+    #print('final state values: ', final_state_values.view(int(len(final_state_values)/env.mod_size),env.mod_size))
+    #print('final actions: ', action_batch[curr_mask_transposed])
+    #final_state_action_values = state_action_values[curr_mask_transposed]
+    #print('final state action values: ', final_state_action_values)#.view(int(len(final_state_action_values)/env.mod_size),1))
+
+    #print('state action values: ', state_action_values)
+    next_state_batch = torch.cat(batch.next_state).view(env.batch_size,len(env.state))
+    next_state_vals = target_net(next_state_batch, goal_batch, 1).detach()
+    next_state_vals[mask_batch] = -float('inf')
+    #print('masked next state values: ', next_state_vals)
+    #print('final next state values: ', next_state_vals[curr_mask_batch])
+
+    max_next_state_vals = next_state_vals.max(1)[0]
+    #print('max next state values: ', next_state_vals)
     done_batch = torch.cat(batch.done)
-    #print('done batch: ', done_batch)
-    #print('action batch: ', action_batch)
-    # Bellman's equation for updating Q values
-    #print('state batch: ', state_batch)
-    state_action_values = policy_net.forward(state_batch, goal_batch, 1).gather(1, action_batch)
-    #print('state action values: ', state_action_values) # values for selecting action at each state
-    next_state_vals = target_net.forward(next_state_batch, goal_batch, 1).max(1)[0].detach()
-    #print('reward batch: ', reward_batch)
-    #print('next state values: ', policy_net.gamma * next_state_vals)
-    #print('next state values times done: ', policy_net.gamma * next_state_vals * done_batch)
-    expected_state_action_values = reward_batch + policy_net.gamma * next_state_vals * done_batch
-    #print('expect state action values: ', expected_state_action_values)
+    reward_batch = torch.cat(batch.reward)
+    #print('rewards: ', reward_batch)
+
+    expected_state_action_values = reward_batch + policy_net.gamma * max_next_state_vals * done_batch
+    #print('final expected state action values: ', expected_state_action_values.unsqueeze(1)[curr_mask_transposed])
+    #print('')
     # Computing loss
     loss = F.smooth_l1_loss(state_action_values, expected_state_action_values.unsqueeze(1))
-    policy_net.optimizer.zero_grad()
+    optimizer.zero_grad()
 
     # back-propagate loss
     loss.backward()
     for param in policy_net.parameters():
         param.grad.data.clamp_(-1, 1)
     # optimizer step
-    policy_net.optimizer.step()
+    optimizer.step()
+    scheduler.step()
+
 
     return loss.item()
 
 if __name__ == '__main__':
     # parameters
     lr = 2e-3
+    LR_DECAY = 1000
     gamma = .999
     train_episodes = 1000
     test_episodes = 50
-    val_goals = [[.1, .1, .1], [.3, .3, .3], [.5, .5, .5]]
+    val_goals = [[.1, .1, .1], [.2, .2, .2], [.3, .3, .3], [.4, .4, .4]]
 
-    env = env(arm_size=8) # initialize environment
+    env = env(arm_size=10) # initialize environment
 
     # initialize policy network where we are frequently updating weights
     policy_net = DQN(len(env.state), env.mod_size,lr,gamma, len(env.goal))
@@ -216,57 +256,74 @@ if __name__ == '__main__':
     # initialize target network where we are periodically updating weights
     target_net = DQN(len(env.state), env.mod_size,lr,gamma, len(env.goal))
     target_net.load_state_dict(policy_net.state_dict()) # sets target net equal to policy net to begin
+    target_net.eval()
 
-    buffer = ReplayMemory(150)
+    optimizer = optim.RMSprop(policy_net.parameters(), lr=lr)
+    lambda1 = lambda ep: math.exp(-ep / LR_DECAY)
+    scheduler = optim.lr_scheduler.LambdaLR(optimizer, lambda1)
+
+    buffer = ReplayMemory(1000)
     # training
     total_loss = 0
     dist = 0
-    TARGET_UPDATE = 50
-    LR_DECAY = 1000
+    TARGET_UPDATE = 100
     for ep in range(train_episodes):
+        env.reset()
         print('ep: ', ep)
-        print('goal: ', env.goal)
+        #print('goal: ', env.goal)
         # every 100 episodes, do validation checks
-        if (ep + 100) % 100 == 0:
+        if ep % 25 == 0:
             validation(env)
         for curr in range(0, len(env.state), env.mod_size):
-            action = choose_action(policy_net, env.state, env.mod_size, env.prev_action, env.goal, steps_done)
-            next_a, r, dist, done = env.step(env.state,curr, env.goal, action)
-            print('PUSHING TO BUFFER:')
-            print('state: ', env.state)
-            print('action: ', torch.tensor(np.array([action])).type(torch.LongTensor))
-            print('reward: ', torch.tensor(np.array([r])).type(torch.FloatTensor))
+            action = choose_action(policy_net, env.state, env.mod_size, env.prev_action, env.goal, ep)
+            next_a, r, dist, done, mask = env.step(env.state,curr, env.goal, action)
+            #curr_mask = torch.zeros(1, env.mod_size, dtype=torch.bool)
+            #if done:
+            #    curr_mask[0][0:env.mod_size] = True
+            #print('PUSHING TO BUFFER:')
+            #print('state: ', env.state)
+            #print('action: ', torch.tensor(np.array([action])).type(torch.LongTensor))
+            #print('next state: ', next_a)
+            #print('reward: ', torch.tensor(np.array([r])).type(torch.FloatTensor))
+            #print('goal: ', env.goal)
+            #print('done: ', torch.tensor(np.array([1 - done])).type(torch.LongTensor))
             buffer.push(env.state, torch.tensor(np.array([action])).type(torch.LongTensor), next_a,
-                        torch.tensor(np.array([r])).type(torch.FloatTensor), env.goal, torch.tensor(np.array([1 - done])).type(torch.LongTensor))
+                        torch.tensor(np.array([r])).type(torch.FloatTensor), env.goal, torch.tensor(np.array([1 - done])).type(torch.LongTensor), mask)
             if done:
                 if env.failed:
-                    print('')
+                    #print('')
                     print('failed')
                     print('goal point was: ' + str(env.goal) + " while end eff position was " + str(env.buffer_goal))
                     print('distance was: ', dist)
                     env.sequence.append((env.state, torch.tensor(np.array([action])).type(torch.LongTensor), next_a,
-                                        torch.tensor(np.array([1.0])).type(torch.FloatTensor), done))
+                                        torch.tensor(np.array([1.0])).type(torch.FloatTensor), done, mask))
                     for part in env.sequence:
-                        print('PUSHING TO BUFFER:')
-                        print('state: ', part[0])
-                        print('action: ', part[1])
-                        print('reward: ', part[3])
-                        print('new goal of: ', env.buffer_goal)
-                        buffer.push(part[0], part[1], part[2], part[3], env.buffer_goal, torch.tensor(np.array([1 - part[4]])).type(torch.LongTensor))
-                l = optimize_model(buffer, env, policy_net, target_net)
+                        #print('PUSHING TO BUFFER:')
+                        #print('state: ', part[0])
+                        #print('action: ', part[1])
+                        #print('next state: ', part[2])
+                        #print('reward: ', part[3])
+                        #print('goal: ', env.buffer_goal)
+                        #print('done: ', torch.tensor(np.array([1 - part[4]])).type(torch.LongTensor))
+                        #print('new goal of: ', env.buffer_goal)
+                        buffer.push(part[0], part[1], part[2], part[3], env.buffer_goal, torch.tensor(np.array([1 - part[4]])).type(torch.LongTensor), part[5])
                 #policy_net.lr = policy_net.lr*math.exp(-ep/LR_DECAY)
-                total_loss += l
+                else:
+                    print('succeeded')
+                    print('goal point was: ' + str(env.goal))
+                    print('distance: ', dist)
                 writer.add_scalar('Distance/train', dist, ep)
                 break
             else:
                 env.sequence.append((env.state, torch.tensor(np.array([action])).type(torch.LongTensor), next_a,
-                        torch.tensor(np.array([r])).type(torch.FloatTensor), done))
+                        torch.tensor(np.array([r])).type(torch.FloatTensor), done, mask))
+            l = optimize_model(buffer, env, policy_net, target_net, optimizer)
+            total_loss += l
             env.state = copy.deepcopy(next_a)
 
         if ep % TARGET_UPDATE == 0:
             target_net.load_state_dict(policy_net.state_dict())
 
-        env.reset()
         writer.add_scalar('Total Loss/train', total_loss,ep)
 
     # testing
@@ -275,8 +332,9 @@ if __name__ == '__main__':
         print('goal: ', env.goal)
         for curr in range(0,len(env.state), env.mod_size):
             action = env.test_step(target_net,curr)
+            print('action: ', action)
             if action == env.mod_size - 1:
-                final_dist = term_reward(env.state, env.mod_size, env.goal, curr, modules)
+                final_dist = term_reward(env.state, env.mod_size, env.goal, curr, modules, env)
                 print('final distance: ', final_dist[0])
                 writer.add_scalar('Distance/test', final_dist[0], test)
                 break
--- pybullet_sim.py
+++ pybullet_sim.py
@@ -4,7 +4,7 @@ import numpy as np
 import math
 import time
 
-def sim(goal):
+def sim(goal, epsilon):
     physicsClient = p.connect(p.DIRECT)  # or p.DIRECT for non-graphical version, p.DIRECT is faster
     p.setAdditionalSearchPath(pybullet_data.getDataPath())
     cubeStartPos = [0,0,0]
@@ -14,18 +14,23 @@ def sim(goal):
     finalJoint = p.getNumJoints(robotId)
 
     # inverse kinematics
-    angles = p.calculateInverseKinematics(robotId, finalJoint - 1, goal)
 
     #p.setRealTimeSimulation(1) # don't need if doing p.DIRECT for server, but do need for p.GUI
-
+    #endEffPos = 0
+    #dist = 0
     # forward kinematics
-    for i in range(len(angles)):
-        p.resetJointState(robotId, 3 * i + 2, angles[i])
-    #for joint in range(finalJoint):
+    for i in range(20):
+        angles = p.calculateInverseKinematics(robotId, finalJoint - 1, goal)#)
+        for i in range(len(angles)):
+            p.resetJointState(robotId, 3 * i + 2, angles[i])
     endEffPos = p.getLinkState(robotId, finalJoint - 1)[0]
+    dist = np.linalg.norm(np.array([a_i - b_i for a_i, b_i in zip(endEffPos, goal)]))
+        #print('iteration ', j)
+        #print('distance ', dist)
+
+    #for joint in range(finalJoint):
 
     # determine dist from tip of EE to goal
-    dist = np.linalg.norm(np.array([a_i - b_i for a_i, b_i in zip(endEffPos, goal)]))
     # delete arm from simulation
     p.disconnect()
     #print('dist: ', dist)
